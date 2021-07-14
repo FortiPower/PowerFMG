85ab71b844a22aaffa46765505039a35326799bc
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-07-14 15:28:03 +0200

    Confirm: Add Confirm-FMGAddress
    From PowerFGT...

---
+++ PowerFMG/Private/Confirm.ps1
@@ -0,0 +1,31 @@
+
+#
+# Copyright 2021, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+Function Confirm-FMGAddress {
+
+    Param (
+        [Parameter (Mandatory = $true)]
+        [object]$argument
+    )
+
+    #Check if it looks like an Address element
+
+    if ( -not ( $argument | get-member -name name -Membertype Properties)) {
+        throw "Element specified does not contain a name property."
+    }
+    if ( -not ( $argument | get-member -name uuid -Membertype Properties)) {
+        throw "Element specified does not contain an uuid property."
+    }
+    if ( -not ( $argument | get-member -name type -Membertype Properties)) {
+        throw "Element specified does not contain a type property."
+    }
+    if ( -not ( $argument | get-member -name country -Membertype Properties)) {
+        throw "Element specified does not contain a country property."
+    }
+
+    $true
+
+}
